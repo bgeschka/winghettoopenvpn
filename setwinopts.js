b9c104545b169e180c3fbd13d96e0cb1cea2451d
Björn Geschka <bjoern@geschka.org>
2021-05-06 23:10:01 +0200

    only add icon if present

--- setwinopts.js
+++ setwinopts.js
@@ -1,8 +1,9 @@
 const rcedit = require('rcedit')
+const fs = require('fs')
 //when invoked with nodejs, argv2 is path to exe, argv3 is path to icon
 var opts = {
 	"requested-execution-level": "requireAdministrator"
 };
-if(process.argv[3]) opts.icon = process.argv[3];
+if(process.argv[3] && fs.existsSync(process.argv[3])) opts.icon = process.argv[3];
 
 rcedit(process.argv[2], opts);
